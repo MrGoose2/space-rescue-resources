48461e892b5a9661f9c017b681c1fa36fdba3c79
LordGoose1 <128349507+MrGoose2@users.noreply.github.com>
2026-07-27 13:43:20 +1000

    Kept Ship in Room

--- Objects/Ship.py
+++ Objects/Ship.py
@@ -1,9 +1,9 @@
-from GameFrame import RoomObject
+from GameFrame import RoomObject, Globals
 import pygame
 
 class Ship(RoomObject):
     """
-    A cclass for the player's avitar (the Ship)
+    A class for the player's avitar (the Ship)
     """
 
     def __init__(self, room, x, y):
@@ -28,3 +28,19 @@ class Ship(RoomObject):
             self.y_speed -= 5
         elif key[pygame.K_s]:
             self.y_speed += 5
+
+    def keep_in_room(self):
+        """
+        Keeps the ship inside the room
+        """
+        if self.y < 0:
+            self.y = 0
+        elif self.y + self.height> Globals.SCREEN_HEIGHT:
+            self.y = Globals.SCREEN_HEIGHT - self.height
+
+    def step(self):
+        """
+        Determine what happens to the Ship on each tick of the game clock
+        """
+        self.keep_in_room()
+
